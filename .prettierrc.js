b4a12a23aad61895fa2489b3814ccd4a71737640
CosLynx AI <drixisgod@gmail.com>
2024-12-30 00:47:20 +0530

    generated file: `.prettierrc.js`

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
